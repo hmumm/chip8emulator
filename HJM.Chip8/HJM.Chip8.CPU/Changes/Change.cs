b81c71f18ccd355976528afaa42dee294da33978
Hayden Mumm <28241556+hmumm@users.noreply.github.com>
2020-10-29 18:51:02 -0500

    remove change description never really made any sense

--- HJM.Chip8/HJM.Chip8.CPU/Changes/Change.cs
+++ HJM.Chip8/HJM.Chip8.CPU/Changes/Change.cs
@@ -10,7 +10,6 @@ namespace HJM.Chip8.CPU.Changes
     /// <typeparam name="T">Type of the value to be changed</typeparam>
     public class Change<T>
     {
-        public string Description { get; set; } = string.Empty;
 #pragma warning disable CS8601 // Possible null reference assignment.
         public T NewValue { get; set; } = default;
 #pragma warning restore CS8601 // Possible null reference assignment.
